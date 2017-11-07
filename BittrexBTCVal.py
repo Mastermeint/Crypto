6636caec7b5db8c34db963f68ce35437683d3a6b
Mastermeint <surfdude.jr@gmail.com>
2017-11-07 23:39:36 +0100

    clean names and working current Bittrex and Kraken BTC values

---
+++ BittrexBTCVal.py
@@ -0,0 +1,12 @@
+import json
+from bittrex.bittrex import Bittrex
+import pprint
+
+bit = Bittrex(None, None)
+actual = bit.get_orderbook('USDT-BTC')
+buys = actual['result']['buy']
+sell = actual['result']['sell']
+actual['result']['buy'] = buys[0:10]
+actual['result']['sell'] = sell[0:10]
+
+pprint.pprint(actual)
